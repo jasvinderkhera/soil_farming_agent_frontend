08d20082021f42791af53dbab998176893ef1ee3
jasvinderkhera <ankitkhera15@gmail.com>
2024-12-21 19:03:57 +0530

    forgot password page added

--- src/components/userAuth/userAuth.jsx
+++ src/components/userAuth/userAuth.jsx
@@ -5,6 +5,7 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  sendPasswordResetEmail
 } from "firebase/auth";
 import { ref, set, get } from "firebase/database";
 import { auth, realtimeDb } from "../../firebase/firebase"; 
@@ -21,6 +22,17 @@ const UserAuth = () => {
   const [isRegistering, setIsRegistering] = useState(false);
   const dispatch = useDispatch()
   const navigate = useNavigate(); // React Router navigation
+  const [resetEmail, setResetEmail] = useState("");
+  const [loginPage, setLoginPage] = useState('login')
+
+  const handleForgotPassword = async () => {
+    try {
+      await sendPasswordResetEmail(auth, resetEmail);
+      alert("Password reset email sent! Check your inbox.");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   
 
   const handleAuth = (e) => {
@@ -89,7 +101,7 @@ const UserAuth = () => {
     <div>
       <div className="topContainer container h-100 d-flex flex-column justify-content-center align-items-center">
         <h2 className="text-center dark-text my-4"> Welcome to KrishiSahayak</h2>
-      <form onSubmit={handleAuth} className="loginForm p-4">
+     { loginPage === "login" ? <form onSubmit={handleAuth} className="loginForm p-4">
       <h2 className="text-center mb-3">{isRegistering ? "Register" : "Login"}</h2>
       <div className="mb-3">
       <input
@@ -115,7 +127,32 @@ const UserAuth = () => {
           ? <p>Already have an account? <span className="text-primary">Login</span></p>
           : <p>Don't have an account? <span className="text-primary">Register</span></p>}
       </p>
-    </form>
+    </form> :
+    <form action="" className="loginForm p-4">
+      <h4 className=" text-color mb-4 text-center">
+                Don't worry reset your password 
+              </h4>
+              <input
+         type="email"
+         placeholder="Enter your email"
+         value={resetEmail}
+         onChange={(e) => setResetEmail(e.target.value)}
+         className='form-control px-4 rounded-full mb-8 loginInput'
+       />
+         <p className="text-center">
+                {" "}
+                <button
+                  className="btn px-5 py-2 mt-3 btn-primary d-inline-block"
+                  onClick={handleForgotPassword} 
+                >
+                  Reset Password
+                </button>
+              </p>
+              </form>
+}
+            
+            
+    <p className="text-primary text-center" type="button" onClick={()=>{ loginPage === "login" ? setLoginPage("forgot") : setLoginPage("login")}}> {loginPage === "login" ? " Forgot Password ?" : "Login"}</p>
       </div>
     </div>
   );
